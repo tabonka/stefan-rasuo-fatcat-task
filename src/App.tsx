b13474f9fa8c8488bd7da2f9e178630c6591e817
tabonka <rasuostefan@gmail.com>
2024-07-20 14:51:02 +0200

    Created a generic List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 import '@homework-task/styles.css';
+import { z } from 'zod';
+
 import { Landing } from '@homework-task/components/landing/Landing';
+import List from '@homework-task/components/List';
 
 export default function App() {
+    const userSchema = z.object({
+        id: z.number(),
+        name: z.string(),
+        username: z.string(),
+        email: z.string().email('invalid mail'),
+    });
+
+    const cardContent = (data: z.infer<typeof userSchema>) => (
+        <div className="flex flex-col p-2 shadow rounded">
+            <div className="flex flex-row font-medium text-lg mb-2">
+                #<span>{data.id}</span>-<span>{data.name}</span>
+            </div>
+            <span>
+                <span className="font-medium">Username:</span> {data.username}
+            </span>
+            <span>
+                <span className="font-medium">email:</span> {data.email}
+            </span>
+        </div>
+    );
+
     return (
         <main>
+            <List
+                apiEndpoint="https://jsonplaceholder.typicode.com/users"
+                resSchema={userSchema}
+                cardContent={cardContent}
+            />
             <Landing />
         </main>
     );
---
+++ src/components/List.tsx
@@ -0,0 +1,62 @@
+import { ReactNode, useEffect, useState } from 'react';
+
+import { z } from 'zod';
+
+import { Skeleton } from '@homework-task/components/skeleton';
+
+type ListProps<T extends z.ZodTypeAny> = {
+    apiEndpoint: string;
+    resSchema: T;
+    cardContent: (data: z.infer<T>) => ReactNode;
+};
+
+const List = <T extends z.ZodTypeAny>({
+    apiEndpoint,
+    resSchema,
+    cardContent,
+}: ListProps<T>) => {
+    const [error, setError] = useState<boolean>(false);
+    const [listData, setListData] = useState<z.infer<T>[]>([]);
+
+    useEffect(() => {
+        if (apiEndpoint) {
+            fetch(apiEndpoint, {
+                method: 'GET',
+            })
+                .then((res) => {
+                    res.json()
+                        .then((data) => {
+                            const parseData = z
+                                .array(resSchema)
+                                .safeParse(data);
+                            if (parseData.success) {
+                                setListData(parseData.data);
+                                setError(false);
+                            } else {
+                                setError(true);
+                            }
+                        })
+                        .catch(() => {
+                            setError(true);
+                        });
+                })
+                .catch(() => {
+                    setError(true);
+                });
+        } else {
+            setError(true);
+        }
+    }, [apiEndpoint, listData, resSchema]);
+
+    return listData.length > 0 ? (
+        <div className="flex flex-row flex-wrap w-full h-full gap-4Username">
+            {listData.map((item) => cardContent(item))}
+        </div>
+    ) : error ? (
+        <>There was an error fetching data.</>
+    ) : (
+        <Skeleton className="h-12 w-12 w-full rounded">Loading...</Skeleton>
+    );
+};
+
+export default List;
